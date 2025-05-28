452a0df98412bd5358d557a676bc4a51666db128
Žiga Vehovec <ziga.veho@gmail.com>
2025-05-28 12:18:22 +0200

    Add publishing config (#2)
    * Add publishing config
    
    * Merge fixes

--- mediatailor/build.gradle.kts
+++ mediatailor/build.gradle.kts
@@ -5,14 +5,29 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
+    alias(libs.plugins.jfrog.artifactory)
 }
 
+val groupId: String by rootProject
+val versionName: String by rootProject
+val artifactoryUser: String? by rootProject
+val artifactoryPassword: String? by rootProject
+
+if (artifactoryUser == null || artifactoryPassword == null) logger.warn(
+    "Warning: Missing credentials to fetch and publish from Artifactory! " +
+            "Please add them to ~/.gradle/gradle.properties"
+)
+
 android {
+    val minSdkVersion: String by rootProject
+    val compileSdkVersion: String by rootProject
+
     namespace = "com.bitmovin.player.integration.mediatailor"
-    compileSdk = 35
+    compileSdk = compileSdkVersion.toInt()
 
     defaultConfig {
-        minSdk = 21
+        minSdk = minSdkVersion.toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -37,6 +52,11 @@ android {
     testOptions {
         unitTests.all {
             it.useJUnitPlatform()
+        }
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
         }
     }
 }
@@ -67,5 +87,40 @@ tasks.withType<KotlinJvmCompile>().all {
     compilerOptions {
         // Enable explicit API for production code
         freeCompilerArgs.add("-Xexplicit-api=strict")
+    }
+}
+
+
+project.afterEvaluate {
+    publishing {
+        publications {
+            val projectGroupId = groupId
+            create("release", MavenPublication::class) {
+                from(components["release"])
+                groupId = projectGroupId
+                artifactId = project.name
+                version = versionName
+            }
+        }
+    }
+}
+
+artifactory {
+    setContextUrl("https://bitmovin.jfrog.io/bitmovin")
+    publish {
+        repository {
+            repoKey = if (version.toString().endsWith("SNAPSHOT")) {
+                "libs-snapshot-local"
+            } else {
+                "libs-release-local"
+            }
+            username = artifactoryUser
+            password = artifactoryPassword
+        }
+        defaults {
+            publications(publishing.publications["release"])
+            setPublishArtifacts(true)
+            setPublishIvy(false) // Publish generated Ivy descriptor files to Artifactory (true by default)
+        }
     }
 }
